b44e47355af2e950d89a999e37085206fc3acd86
AbrarSaud <abrar.sauod0@gmail.com>
2024-04-15 23:54:35 +0300

    adding functions of  Create Task

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,28 @@
-import React ,{useState} from "react";
-import CreateTask from "../modals/CreateTask"
+import React, { useState } from "react";
+import CreateTask from "../modals/CreateTask";
 
 const TodoList = () => {
   const [modal, setModal] = useState(false);
+  const [taskList, setTaskList] = useState([]);
   const toggle = () => setModal(!modal);
+  const saveTask = (taskObj) => {
+    let tempList = taskList;
+    tempList.push(taskObj);
+    setTaskList(tempList);
+    setModal(false)
+  };
 
   return (
     <>
       <div className="header text-center">
         <h3>Todo List</h3>
-        <button className="btn btn-primary mt-2">Create Task</button>
+        <button className="btn btn-primary mt-2" onClick={() => setModal(true)}>
+          Create Task
+        </button>
       </div>
-      <div className="task-container"></div>
+      <div className="task-container">{taskList.map((obj)=> <li>{obj.Name}</li>)}</div>
 
-      <CreateTask toggle={toggle} modal={modal}/>
+      <CreateTask toggle={toggle} modal={modal} saveTask={saveTask} />
     </>
   );
 };
--- src/modals/CreateTask.js
+++ src/modals/CreateTask.js
@@ -1,29 +1,65 @@
-import React from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-const CreateTask = ({modal ,toggle}) => {
-  return <div>
-    <Modal isOpen={modal} toggle={toggle} >
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+const CreateTask = ({ modal, toggle, saveTask }) => {
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+  const handChange = (e) => {
+    // const name =e.target.name
+    // const value =e.target.value
+    const { name, value } = e.target;
+    if (name === "taskName") {
+      setTaskName(value);
+    } else {
+      setDescription(value);
+    }
+  };
+  const haandleSave = () => {
+    let taskObj = {};
+    taskObj["Name"] = taskName;
+    taskObj["Description"] = description;
+    saveTask(taskObj);
+  };
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Create Task</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <form>
+            <div className="form-group">
+              <label>Task Name :</label>
+              <input
+                type="text"
+                className="form-control"
+                value={taskName}
+                onChange={handChange}
+                name="taskName"
+              />
+            </div>
+            <div className="form-group">
+              <label>Description:</label>
+
+              <textarea
+                rows={5}
+                className="form-control"
+                value={description}
+                onChange={handChange}
+                name="description"
+              ></textarea>
+            </div>
+          </form>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{' '}
+          <Button color="primary" onClick={haandleSave}>
+            Create
+          </Button>{" "}
           <Button color="secondary" onClick={toggle}>
             Cancel
           </Button>
         </ModalFooter>
       </Modal>
-  </div>;
+    </div>
+  );
 };
 
 export default CreateTask;
